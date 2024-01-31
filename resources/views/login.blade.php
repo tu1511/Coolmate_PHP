f8f1a87d57bd61b8f37d7a9f7907c0677b371626
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-01-31 23:59:57 +0700

    push code

--- resources/views/login.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Trang login</h1>
-<form action="/check_login" method="POST">
-    <input type="text" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-    @csrf
-</form>
